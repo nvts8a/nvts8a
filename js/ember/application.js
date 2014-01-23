ba042e9f732ca997329773e74cd39a2707d21a7e
Steven Justin <nvts8a@gmail.com>
2014-01-22 23:53:43 -0500

    experience now displaying formated start and end dates, create bound helper to do so

--- js/ember/application.js
+++ js/ember/application.js
@@ -3,3 +3,15 @@ window.App = Ember.Application.create();
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+// Bound Helpers for App
+Ember.Handlebars.registerBoundHelper('formatExperienceDate', function(date) {
+	var experienceDate = 'Present';
+	var momentExperienceDate = moment( date ).format( 'MMMM YYYY' );
+	var momentCurrentDate = moment().format( 'MMMM YYYY' );
+
+	if( momentExperienceDate.localeCompare( momentCurrentDate ) ) {
+		experienceDate = momentExperienceDate;	
+	}
+
+	return experienceDate;
+});
