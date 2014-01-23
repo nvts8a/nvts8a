9c9cb435b7ee8079e6474c13c05a12d8061cd118
Steven Justin <nvts8a@gmail.com>
2014-01-23 00:42:10 -0500

    skills inflection on year depending the number of years, using new bound helper

--- js/ember/application.js
+++ js/ember/application.js
@@ -4,6 +4,16 @@ window.App = Ember.Application.create();
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 // Bound Helpers for App
+Ember.Handlebars.registerBoundHelper('inflectionSkillDuration', function(duration) {
+	var inflection = 'year';
+
+	if( duration > 1 ) {
+		inflection = 'years';
+	}
+
+	return inflection;
+});
+
 Ember.Handlebars.registerBoundHelper('formatExperienceDate', function(date) {
 	var experienceDate = 'Present';
 	var momentExperienceDate = moment( date ).format( 'MMMM YYYY' );
